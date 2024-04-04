7d5239710806c3ba471d1becdcdf48b15df25440
薛业上 <2879322503@qq.com>
2024-04-05 00:14:07 +0800

    fix(kv-raft): 注释掉rpctimeout，去掉todo

--- raft/common/common.go
+++ raft/common/common.go
@@ -9,9 +9,9 @@ const (
 
 	ElectionBaseTimeout     = 150 // 选举基准超时时间（毫秒）
 	ElectionMaxExtraTimeout = 150 // 选举额外最大超时时间（毫秒）
-	HeartbeatTimeout        = 25  // 心跳时间超时时间（毫秒）TODO 待确定
+	HeartbeatTimeout        = 25  // 心跳时间超时时间（毫秒）
 
-	RpcTimeout = 10 // rpc超时时间，设置100毫秒
+	//RpcTimeout = 10 // rpc超时时间，设置100毫秒
 )
 
 type LogEntry struct {
